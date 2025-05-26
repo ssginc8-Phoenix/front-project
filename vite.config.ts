19141d81f536278bcd052c2ba597abb2460d2222
Soomin <o3o_121@naver.com>
2025-05-26 17:16:38 +0900

    🔧 Chore: styled-component 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    cssMinify: true,
+    ssr: false,
+  },
+  ssr: {
+    noExternal: ['styled-components'],
+  },
 });

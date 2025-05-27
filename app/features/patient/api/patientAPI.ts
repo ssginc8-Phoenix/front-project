8ce4dba8e049866e1404cf1e00672e29bc6498f8
Soomin <o3o_121@naver.com>
2025-05-27 17:20:42 +0900

    💬 Comment: API 설명 추가

--- app/features/patient/api/patientAPI.ts
+++ app/features/patient/api/patientAPI.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const HOST = 'http://localhost:8080/api/v1/patients';
 
+/**
+ * 모든 환자 조회 API
+ */
 export const getPatientList = async () => {
   const res = await axios.get(HOST);
   return res.data;

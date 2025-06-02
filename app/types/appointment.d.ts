63ed0424192b05ae1a50e6ec8dbebe25771fd953
Soomin <o3o_121@naver.com>
2025-06-02 16:53:20 +0900

    ✨ Feat: 인터페이스 및 타입 정의 추가

--- app/types/appointment.d.ts
+++ app/types/appointment.d.ts
@@ -9,3 +9,41 @@ export interface AppointmentRequest {
   paymentType: string;
   appointmentTime: string;
 }
+
+export interface Appointment {
+  appointmentId: number;
+  hospitalId: number;
+  doctorId: number;
+  patientGuardianId: number;
+
+  hospitalName: string;
+  doctorName: string;
+  patientName: string;
+
+  symptom: string;
+  question: string | null;
+
+  appointmentTime: string;
+  appointmentType: string;
+  paymentType: string;
+  status: string;
+
+  createdAt: string;
+}
+
+export interface AppointmentList {
+  appointmentId: number;
+  hospitalId: number;
+  doctorId: number;
+  patientGuardianId: number;
+
+  hospitalName: string;
+  doctorName: string;
+  patientName: string;
+
+  appointmentTime: string;
+  appointmentType: string;
+  status: string;
+}
+
+export type AppointmentListPage = Page<AppointmentList>;

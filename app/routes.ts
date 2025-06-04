828d9fd59cfd1ff16a0972164bf112c157de0719
Soomin <o3o_121@naver.com>
2025-06-04 23:18:32 +0900

    ♻️ Refactor: 주석 해제

--- app/routes.ts
+++ app/routes.ts
@@ -18,10 +18,10 @@ export default [
 
   route('/reviews', 'layout/ReviewLayout.tsx', [
     route('me/*', 'features/reviews/pages/ReviewMyListPage.tsx', [
-      // route('new', 'features/reviews/pages/ReviewCreatePage.tsx'),
-      // route(':reviewId/edit', 'features/reviews/pages/ReviewEditPage.tsx'),
-      // route(':reviewId/delete', 'features/reviews/pages/ReviewDeletePage.tsx'),
+      route('new', 'features/reviews/pages/ReviewCreatePage.tsx'),
+      route(':reviewId/edit', 'features/reviews/pages/ReviewEditPage.tsx'),
+      route(':reviewId/delete', 'features/reviews/pages/ReviewDeletePage.tsx'),
     ]),
-    // route('hospital/:hospitalId', 'features/reviews/pages/ReviewAllListPage.tsx'),
+    route('hospital/:hospitalId', 'features/reviews/pages/ReviewAllListPage.tsx'),
   ]),
 ] satisfies RouteConfig;

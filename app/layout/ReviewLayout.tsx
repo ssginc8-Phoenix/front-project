398e34645c4694cd2b6c9d2af554d621e672b771
jiiinchoi <hanuel001029@naver.com>
2025-06-04 23:01:12 +0900

    💄 Style: HeaderLayout -> Header 변경

--- app/layout/ReviewLayout.tsx
+++ app/layout/ReviewLayout.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router';
-import HeaderLayout from '~/layout/HeaderLayout';
 import FooterLayout from '~/layout/FooterLayout';
+import Header from '~/layout/Header';
 
 export default function ReviewLayout() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <HeaderLayout />
+      <Header />
 
       <main style={{ flex: 1, padding: '1rem' }}>
         <Outlet />

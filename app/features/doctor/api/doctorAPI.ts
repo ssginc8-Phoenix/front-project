e88a30575ad19aeeb22f2df8076eacf758d90396
jaehoney01 <wogjse0321@naver.com>
2025-06-11 09:57:36 +0900

    ✨ Feat : 의사 일정 관리

--- app/features/doctor/api/doctorAPI.ts
+++ app/features/doctor/api/doctorAPI.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import type { CalendarRequest, DoctorCalendarResponse } from '~/features/doctor/types/calendar';
 
 const HOST = 'http://localhost:8080/api/v1/doctors';
 
@@ -19,6 +20,16 @@ export const getDoctorList = async (hospitalId: number) => {
 export const getDoctorSchedules = async (doctorId: number) => {
   const res = await axios.get(`${HOST}/${doctorId}/schedules`, {
     withCredentials: true,
+  });
+  return res.data;
+};
+
+export const getDoctorCalendar = async (
+  params: CalendarRequest,
+): Promise<DoctorCalendarResponse> => {
+  const res = await axios.get(`http://localhost:8080/api/v1/calendar/doctor`, {
+    params,
+    withCredentials: true, // 로그인 인증 쿠키 포함
   });
   return res.data;
 };

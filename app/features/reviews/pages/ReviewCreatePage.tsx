d871015b2aea9cb27db4624aab05ed8f12dacbea
jiiinchoi <hanuel001029@naver.com>
2025-06-15 02:40:17 +0900

    🗑️ Remove: 안 쓰는 파일 삭제

--- app/features/reviews/pages/ReviewCreatePage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ReviewEditPage() {
-  return (
-    <div>
-      <h1>리뷰 생성 페이지</h1>
-    </div>
-  );
-}
--- app/features/reviews/pages/ReviewDeletePage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ReviewDeletePage() {
-  return (
-    <div>
-      <h1>리뷰 삭제 페이지</h1>
-    </div>
-  );
-}

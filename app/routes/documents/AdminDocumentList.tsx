e81a0628ad4aa2fd4d5567081bbb02a9839b8df8
jaehoney01 <wogjse0321@naver.com>
2025-06-24 09:40:38 +0900

    ✨ Feat : 서류 발급 관리자 페이지

---
+++ app/routes/documents/AdminDocumentList.tsx
@@ -0,0 +1,5 @@
+import AdminInsuranceDocumentsPage from '~/features/documents/page/AdminInsuranceDocumentsPage';
+
+export default function AdminDocumentList() {
+  return <AdminInsuranceDocumentsPage />;
+}
